87e992652b1532e644c2d2d362acab0e96ddaa2b
Diogo Soares <dg@diogosoares.com.br>
2024-04-26 00:56:44 -0300

    :white_check_mark: Code Review: Adjust in footer info.

--- resources/views/layouts/_partials/footer.blade.php
+++ resources/views/layouts/_partials/footer.blade.php
@@ -3,7 +3,7 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col-md-6">
-                                <script>document.write(new Date().getFullYear())</script> © App
+                                <script>document.write(new Date().getFullYear())</script> © DiogoSoares.com.br. Todos os direitos reservados.
                             </div>
                             <div class="col-md-6">
                                 <div class="text-md-end footer-links d-none d-md-block">
